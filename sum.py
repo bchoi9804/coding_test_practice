b30fffa1d37b0f438ef3e8da7207992523045eb9
bchoi9804 <bchoi9804@gamil.com>
2022-05-29 21:27:33 +0900

    0529

---
+++ sum.py
@@ -0,0 +1,14 @@
+def solution(a,b):
+    sum = 0
+    if a > b:
+        for i in range(b, a+1):
+            sum += i
+        return sum
+    elif a < b:
+        for i in range(a, b+1):
+            sum += i
+        return sum
+    else:
+        return a
+
+print(solution(3, 5))                        
